f6aaf64bcf32b349721b32884f674774270500e5
Italo A <italoaurelior@gmail.com>
2023-01-21 18:03:44 -0300

    feat: mariadb configs

---
+++ srcs/requirements/mariadb/tools/create_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE wordpress;
+CREATE USER 'itaureli'@'%' IDENTIFIED BY '123123';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'itaureli'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root123123';
