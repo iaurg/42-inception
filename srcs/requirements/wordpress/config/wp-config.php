cb0d0c982f73eca21f45c88c82d4374fa4c4733c
Italo A <italoaurelior@gmail.com>
2023-01-25 06:57:28 -0300

    refactor: wp config files

--- srcs/requirements/wordpress/config/wp-config.php
+++ srcs/requirements/wordpress/config/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_USER', 'itaureli' );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_PASSWORD', '123123' );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
